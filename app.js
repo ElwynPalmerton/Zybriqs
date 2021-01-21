05e7ab4f29c7c3ba487335c4b268f40a066a3d7d
Elwyn Palmerton <elwyn.palmerton@gmail.com>
2021-01-21 16:55:45 -0500

    no changes

--- app.js
+++ app.js
@@ -183,9 +183,9 @@ app.post("/loadState", (req, res) => {
 //Listening for port.
 let port = process.env.PORT;
 
-if (port == null || port == "") {
-  port = 3000;
-}
+// if (port == null || port == "") {
+//   port = 3000;
+// }
 
 app.listen(port, function () {
   console.log("Server has started.");
